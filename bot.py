ae64115a3111890675167d09660ea78437d0c178
Luis Brose <luisbrose1@gmail.com>
2023-11-11 14:18:48 +0100

    [IMP] Secured token in env var

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import re
 from datetime import datetime, timedelta
+import os
 
 import discord
 from discord.ext import commands
@@ -20,11 +21,11 @@ async def send_message(message, response):
 
 
 def run_discord_bot():
-    bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+    bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
     @bot.event
     async def on_ready():
-        print(f'{bot.user} is now running!')
+        print(f"{bot.user} is now running!")
         try:
             await bot.tree.sync()
         except Exception as e:
@@ -32,7 +33,9 @@ def run_discord_bot():
         await schedule_ping()
 
     @bot.tree.command()
-    async def bump(interaction: discord.Interaction):   # /bump to keep the bot and badge active
+    async def bump(
+        interaction: discord.Interaction,
+    ):  # /bump to keep the bot and badge active
         await interaction.response.send_message("bumpala", ephemeral=True)
 
     @bot.event
@@ -44,13 +47,13 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        if '?configure' in user_message:
+        if "?configure" in user_message:
             param_array = re.findall(r"[^\s']+\b|'[^']*'", str(user_message))
             if len(param_array) < 3:
                 await send_message(
                     message,
                     "use: ?configure [message] [interval]    \nto set up this channel for notifications"
-                    "\nan example would be: ?configure HI seconds=10,minutes=5,hours=10,days=1"
+                    "\nan example would be: ?configure HI seconds=10,minutes=5,hours=10,days=1",
                 )
                 return
 
@@ -70,32 +73,40 @@ def run_discord_bot():
             await send_message(
                 message,
                 "use: ?configure ['message'] [interval]    \nto set up this channel for notifications"
-                "\nan example would be: ?configure 'hello World!' seconds=10,minutes=5,hours=10,days=1"
+                "\nan example would be: ?configure 'hello World!' seconds=10,minutes=5,hours=10,days=1",
             )
 
     @bot.event
     async def on_timer_trigger():
-        if not is_configured:
-            print("bot is not set up")
-            return
-
-        await send_message(message_data,
-                           f"<@{message_data.author.id}>\n" + custom_response)
+        await send_message(
+            message_data, f"<@{message_data.author.id}>\n" + custom_response
+        )
 
     async def schedule_ping():
         while True:
             now = datetime.now()
-            cvargs = dict(re.findall(r'(\w+)=(\d+)', interval))
+            cvargs = dict(re.findall(r"(\w+)=(\d+)", interval))
             cvargs = {k: int(v) for k, v in cvargs.items()}
             try:
                 then = now + timedelta(**cvargs)
             except Exception as e:
                 print(e)
-                then = now + timedelta(days=29)  # default to get notified for the badge
+                then = now + timedelta(
+                    days=29
+                )  # default to get notified for the badge
             wait_time = (then - now).total_seconds()
 
             await asyncio.sleep(wait_time)
             bot.dispatch("timer_trigger")
 
-    bot.run('xyz-yourtoken-xyz')  # <- needs a discord bot token.
-    # THE TOKEN WILL BE COMPROMISED IF IN PLAIN TEXT AND IF THE FILE IS PUBLIC
+    if os.environ.get("NOTI_BOT_SECRET"):
+        bot.run(
+            token=os.environ.get("NOTI_BOT_SECRET"),
+        )
+    else:
+        os.environ["NOTI_BOT_SECRET"] = input(
+            "Please provide the secret for you discord application: ",
+        )
+        bot.run(
+            token=os.environ.get("NOTI_BOT_SECRET"),
+        )
